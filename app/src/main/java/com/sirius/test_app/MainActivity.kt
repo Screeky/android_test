de469fec4273f977cf7dccb5261afeeac5eac90b
Screeky <52076116+Screeky@users.noreply.github.com>
2022-07-14 20:29:50 +0300

    bootcamp Screeky

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.sirius.test_app
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val androidButton: Button = findViewById(R.id.install_button)
+        androidButton.setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://store.steampowered.com/app/570/Dota_2/")))
+        }
+
     }
 }
